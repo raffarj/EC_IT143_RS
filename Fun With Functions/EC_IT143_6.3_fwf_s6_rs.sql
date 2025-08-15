b1e380212574e9b1eefb705dd0c2b2c46f501be0
Rafael Lima Sousa <106488682+raffarj@users.noreply.github.com>
2025-08-15 03:20:44 -0300

    Adding deliverable 6.3
    Adding deliverable 6.3

--- Fun With Functions/EC_IT143_6.3_fwf_s6_rs.sql
+++ Fun With Functions/EC_IT143_6.3_fwf_s6_rs.sql
@@ -6,8 +6,8 @@
 
 -- https://stackoverflow.com/questions/5145791/extracting-first-name-and-last-name
 
-SELECT t.ContactName
-	, LEFT(t.ContactName, CHARINDEX(' ', t.ContactName + ' ') - 1) AS first_name
-	, dbo.udf_first_name(t.ContactName) AS first_name2
-	FROM dbo.t_w3_schools_customers AS t
-ORDER BY 1;
+SELECT 
+    ContactName,
+    dbo.fn_GetFirstName(ContactName) AS FirstName_FromUDF,
+    SUBSTRING(ContactName, 1, CHARINDEX(' ', ContactName + ' ') - 1) AS FirstName_AdHoc
+FROM dbo.t_w3_schools_customers;
