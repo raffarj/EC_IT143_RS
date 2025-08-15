0618b8f64713f7b6cb2f33ab92b094ed9d65102b
Rafael Lima Sousa <106488682+raffarj@users.noreply.github.com>
2025-08-15 03:22:11 -0300

    Adding deliverable 6.3
    Adding deliverable 6.3

---
+++ Fun With Functions/EC_IT143_6.3_fwf_s7_rs.sql
@@ -0,0 +1,27 @@
+/*
+***********************************************************************************
+******************************
+NAME: dbo.t_w3_schools_customers
+PURPOSE: Get the first name.
+MODIFICATION LOG:
+Ver Date Author Description
+----- ---------- -----------
+-------------------------------------------------------------------------------
+1.0 05/23/2022 JJAUSSI 1. Built this script for EC IT440
+RUNTIME:
+0m 0s
+NOTES
+:
+This script encontra e separa o primeiro nome do resto do nome
+***********************************************************************************
+*******************************/
+
+WITH FirstNames AS (
+    SELECT 
+        ContactName,
+        dbo.fn_GetFirstName(ContactName) AS FirstName
+    FROM dbo.t_w3_schools_customers
+)
+SELECT *
+FROM FirstNames
+WHERE FirstName IS NULL OR FirstName = '';
