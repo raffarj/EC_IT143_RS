6ff3ba06a27b02e68165853bae5778049bc5f262
Rafael Lima Sousa <106488682+raffarj@users.noreply.github.com>
2025-08-15 03:34:55 -0300

    Adding deliverable 6.3.
    Adding deliverable 6.3.

---
+++ Fun with Triggers/EC_IT143_6.3_fwt_s4_rs.sql
@@ -0,0 +1,18 @@
+
+/
+***********************************************************************************
+******************************
+NAME: EC_IT143_6.3_fwt_s4_rs.sql
+PURPOSE: To know the last modified
+MODIFICATION LOG:
+Ver Date Author Description
+----- ---------- -----------
+-------------------------------------------------------------------------------
+1.0 05/23/2022 RSOUSA 1. Built this script for EC IT143
+RUNTIME:
+0m 0s
+***********************************************************************************
+*******************************/
+UPDATE dbo.t_w3_schools_customers
+SET ContactName = ContactName + ' '
+WHERE CustomerID = 2;
