ea485c013f1cbe390aeea06795b838d02c4ea76f
Rafael Lima Sousa <106488682+raffarj@users.noreply.github.com>
2025-08-15 03:11:46 -0300

    Adding deliverable 6.3
    Adding deliverable 6.3

---
+++ Fun With Functions/EC_IT143_6.3_fwf_s5_rs.sql
@@ -0,0 +1,20 @@
+/
+***********************************************************************************
+******************************
+NAME: dbo.t_w3_schools_customers
+PURPOSE: Get the first name.
+MODIFICATION LOG:
+Ver Date Author Description
+----- ---------- -----------
+-------------------------------------------------------------------------------
+1.0 05/23/2022 JJAUSSI 1. Built this script for EC IT440
+RUNTIME:
+0m 0s
+NOTES
+:
+This is where I talk about what this script is, why I built it, and other stuff...
+***********************************************************************************
+*******************************/
+-- Q:How to extract first name from Contact Name?
+-- A: Well, here is your problem..
+SELECT GETDATE() AS my_date;
