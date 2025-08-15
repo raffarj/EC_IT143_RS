d76b5611e5f4f6858a5ee663ed537d1e1a5f8213
Rafael Lima Sousa <106488682+raffarj@users.noreply.github.com>
2025-08-15 03:15:28 -0300

    Adding deliverable 6.3
    Adding deliverable 6.3

---
+++ Fun With Functions/EC_IT143_6.3_fwf_s6_rs.sql
@@ -0,0 +1,13 @@
+-- Q:How to extract first name from Contact Name?
+
+-- A: Well, here is your problem..
+-- CustomerName = Rafael Sousa -> Rafael
+-- Google search "How to extract first name from combined name tsql stack overflow"
+
+-- https://stackoverflow.com/questions/5145791/extracting-first-name-and-last-name
+
+SELECT t.ContactName
+	, LEFT(t.ContactName, CHARINDEX(' ', t.ContactName + ' ') - 1) AS first_name
+	, dbo.udf_first_name(t.ContactName) AS first_name2
+	FROM dbo.t_w3_schools_customers AS t
+ORDER BY 1;
