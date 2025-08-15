ed4b88aa1d04c72c7226def29cd3c5ba7c2685ee
Rafael Lima Sousa <106488682+raffarj@users.noreply.github.com>
2025-08-15 03:24:55 -0300

    Adding deliverable 6.3Adding deliverable 6.3
    Adding deliverable 6.3

---
+++ Fun With Functions/EC_IT143_6.3_fwf_s8_rs.sql
@@ -0,0 +1,14 @@
+/*
+***********************************************************************************
+NAME: EC_IT143_6.3_fwf_s8_rs.sql
+PURPOSE: Step 8 of 8 - Ask the next question: How do I extract the LAST name?
+MODIFICATION LOG:
+Ver   Date        Author   Description
+----- ----------  -------  ---------------------------------------------
+1.0   08/06/2025  RSousa    Launched next step for last name extraction.
+RUNTIME: Seconds
+***********************************************************************************
+*/
+
+-- QUESTION:
+-- How I can extract the last name in dbo.t_w3_schools_customers?
